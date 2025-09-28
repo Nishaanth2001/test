310cba20d3dac60a8fb089c40bf57726bf691659
Nishaanth2001 <86832615+Nishaanth2001@users.noreply.github.com>
2025-09-28 17:09:54 +0530

    Delete templates/script.js

--- templates/script.js
+++
@@ -1,5 +0,0 @@
-function handleClick() {
-  const input = document.getElementById("textInput").value;
-  const output = document.getElementById("output");
-  output.textContent = `You typed: ${input}`;
-}
