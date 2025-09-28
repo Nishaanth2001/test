5a21a7014c1c7bda34e5e89d18eab276b433747a
Nishaanth2001 <86832615+Nishaanth2001@users.noreply.github.com>
2025-09-28 15:16:49 +0530

    Add files via upload

---
+++ templates/script.js
@@ -0,0 +1,5 @@
+function handleClick() {
+  const input = document.getElementById("textInput").value;
+  const output = document.getElementById("output");
+  output.textContent = `You typed: ${input}`;
+}
