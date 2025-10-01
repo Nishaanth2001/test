5bd1569e5ce8ca24d278e5dc21956cb40d2ab3d0
Nishaanth2001 <86832615+Nishaanth2001@users.noreply.github.com>
2025-10-01 22:29:24 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from Producer import say_hello
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ def index():
 
 @app.route('/send', methods=['POST'])
 def send_message():
-    data = request.get_json()
+    data = request.get_json()            #get the data from front end
     key = data.get('key')
 
     if not key:
@@ -17,11 +17,15 @@ def send_message():
 
     try:
         message = say_hello(key)
-        return jsonify({'message': message, 'status': 'sent'}), 200
+        return jsonify({'message': message, 'status': 'sent'}), 200, redirect(url_for('welcome', ket=key))                      #send the data to frontend
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@app.route('/welcome/<ket>')
+def welcome(ket):
+    return f"Welcome {ket}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
